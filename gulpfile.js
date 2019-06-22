10b2b675c278e151f87322dded3710aea73f5f07
va-z <vasilizadorojnyuk@gmail.com>
2019-06-22 11:40:22 +0500

    Finalizing, commenting, etc.

--- gulpfile.js
+++ gulpfile.js
@@ -40,62 +40,62 @@ const del = require("del");
 // II-1. Рабочие функции (в работе постоянно)
 // II-1-1. Компиляция SCSS кода
 function style() {
-    return gulp.src("./source/scss/**/*.scss")
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(postcss([
-            autoprefixer()
-        ]))
-        .pipe(gulp.dest("./source/css/"))
-        .pipe(server.stream());
+  return gulp.src("./source/scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("./source/css/"))
+    .pipe(server.stream());
 }
 // II-1-2. Запуск локального сервера и автоматическая компиляция и перезагрузка при изменении SCSS кода, JS и HTML кода
 function serve() {
-    server.init({
-        server: {
-            baseDir: "./source/"
-        }
-    });
-    gulp.watch("./source/scss/**/*.scss", style);
-    gulp.watch("./source/js/**/*.js").on("change", server.reload);
-    gulp.watch("./source/*.html").on("change", server.reload);
+  server.init({
+    server: {
+      baseDir: "./source/"
+    }
+  });
+  gulp.watch("./source/scss/**/*.scss", style);
+  gulp.watch("./source/js/**/*.js").on("change", server.reload);
+  gulp.watch("./source/*.html").on("change", server.reload);
 }
 
 // II-2. Минификация изображений (один раз, до начала работы)
 // II-2-1. Минификация изображений
 function images() {
-    return gulp.src("./source/img/**/*.{png,jpg,svg}")
-        .pipe(imagemin([
-            jpegoptim({
-                max: 80,
-                progressive: true
-            }),
-            imagemin.optipng({ optimizationLevel: 3 }),
-            imagemin.svgo({
-                plugins: [
-                    { removeViewBox: false },
-                    { removeTitle: true },
-                    { cleanupNumericValues: { floatPrecision: 0 } }
-                ]
-            })
-        ]))
-        .pipe(gulp.dest("./source/img/"));
+  return gulp.src("./source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      jpegoptim({
+        max: 80,
+        progressive: true
+      }),
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: false },
+          { removeTitle: true },
+          { cleanupNumericValues: { floatPrecision: 0 } }
+        ]
+      })
+    ]))
+    .pipe(gulp.dest("./source/img/"));
 }
 // II-2-2. Создание webp версий растровых картинок
 function createWebp() {
-    return gulp.src("./source/img/**/*.{png,jpg}")
-        .pipe(imagemin([
-            webp({ quality: 75 })
-        ]))
-        .pipe(extReplace(".webp"))
-        .pipe(gulp.dest("./source/img"));
+  return gulp.src("./source/img/**/*.{png,jpg}")
+    .pipe(imagemin([
+      webp({ quality: 75 })
+    ]))
+    .pipe(extReplace(".webp"))
+    .pipe(gulp.dest("./source/img"));
 }
 // II-2-3. Сборка svg-спрайта
 function sprite() {
-    return gulp.src("./source/img/**/icon-*.svg")
-        .pipe(svgstore({ inlineSvg: true }))
-        .pipe(rename("sprite.svg"))
-        .pipe(gulp.dest("./source/img/"));
+  return gulp.src("./source/img/**/icon-*.svg")
+    .pipe(svgstore({ inlineSvg: true }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("./source/img/"));
 }
 //II-3. Экспорт рабочих функций для командной строки
 exports.serve = serve;
@@ -104,59 +104,62 @@ exports.optimizeImages = series(images, createWebp, sprite);
 // Раздел III. Описание функций препроцессора для сборки
 // III-1. Компиляция и минификация CSS
 function styleBuild() {
-    return gulp.src("./source/scss/**/*.scss")
-        .pipe(plumber())
-        .pipe(sourcemap.init())
-        .pipe(sass())
-        .pipe(postcss([
-            autoprefixer()
-        ]))
-        .pipe(csso())
-        .pipe(rename("style.min.css"))
-        .pipe(sourcemap.write("."))
-        .pipe(gulp.dest("./build/css/"))
-        .pipe(server.stream());
+  return gulp.src("./source/scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("./build/css/"))
+    .pipe(server.stream());
 }
 // III-2. Поддержка шаблонизатора <include>
 function htmlInclude() {
-    return gulp.src("./source/*.html")
-        .pipe(posthtml([
-            include()
-        ]))
-        .pipe(gulp.dest("./build/"));
+  return gulp.src("./source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("./build/"));
 }
 // III-3. Минификация HTML
 function htmlMinify() {
-    return gulp.src("./build/*.html")
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest("./build/"));
+  return gulp.src("./build/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("./build/"));
 }
 // III-4. Сжатие JS
 function jsCompress() {
-    return pipeline(
-        gulp.src("./source/**/*.js"),
-        uglify(),
-        gulp.dest("./build/")
-    );
+  return pipeline(
+    gulp.src("./source/**/*.js"),
+    uglify(),
+    gulp.dest("./build/")
+  );
 }
 // III-5. Копирование файлов из source/ в build/
 function copy() {
-    return gulp.src([
-        "./source/fonts/**/*.{woff,woff2}",
-        "./source/js/**",
-        "./source/*.ico"
-    ], {
-            base: "./source/"
-        })
-        .pipe(gulp.dest("./build/"));
+  return gulp.src([
+    "./source/fonts/**/*.{woff,woff2}",
+    "./source/js/**",
+    "./source/*.ico",
+    "./source/img/**/*"
+  ], {
+      base: "./source/"
+    })
+    .pipe(gulp.dest("./build/"));
 }
 // III-6. Удаление лишних файлов
 function clean() {
-    return del("./build/");
+  return del("./build/");
 }
 // III-7. Удаление отдельных svg-иконок, упакованных в спрайт
 function cleanSvg() {
-    return del("./build/img/**/icon-*.svg");
+  return del("./build/img/**/icon-*.svg");
 }
 // III-8. Экспорт команды для сборки
 exports.build = series(clean, copy, styleBuild, cleanSvg, htmlInclude, htmlMinify, jsCompress);
+
+//Идея оказалась не совсем удачной. Сборку билда, как и рекомендовалось раньше, нужно выводить по-другому. В следующем проекте сделаю нормально.
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
-#Responsive cat energy
-A learning project for a cat food shop's web page.
+# Responsive CatNRG
+A learning project somewhat based on a ready template for a fictional cat food store's responsive website. Creative liberites taken with some grid elements, etc. Gulpfile is a mess. First time uploading to github, so that's a cluster too. Bleh.
